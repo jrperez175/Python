7b3fce6002d2988937d1f007d4af43fa3745eb7b
jrperez <jrperez@bancolombia.com.co>
2020-02-24 14:57:43 -0500

    Curso_24Feb2020 15:00

--- Curso.py
+++ Curso.py
@@ -13,9 +13,25 @@ print(msg.split())
 
 print("Elementos de una Lista por medio de un bucle")
 for elemento in listas_nombre:
-    print(elemento)
+    print("Los elementos de la lista son: {0}".format(elemento))
+
+print("Bucle For con ragos")
+x=0
+for index in range(10):
+    x+=10
+    print("El valor de X es: {0}".format(x))
+    
 
 
+print("Tipo de dato range")
+x=range(10)
+print(list(x))
+x=range(2,10)
+print(list(x))
+x=range(2,10,3)
+print(list(x))
+
+ 
 print("Ciclo if define rama a ejecutar")
 if numero1==5:
     print("Sentecia Afirmativa")
@@ -27,3 +43,71 @@ def sumar2Numeros(num1,num2):
     return (num1+num2)
 
 print("Suma de dos numeros por un metodo: " + str(sumar2Numeros(numero1,numero2)))
+
+print("Diccionarios")
+
+estudiante = {
+    "Name":"jhon",
+    "Tipo_Doc":"Cedula",
+    "Num_Doc": 98536175,
+    "Direccion": None
+}
+
+print("----------------------")
+print("Manejo de Excepciones")
+print("----------------------")
+try:
+    Apellido = estudiante["Apellido"]
+except KeyError as error:
+    print("Error buscando apellido")
+    print(error)
+
+print("----------------------")
+print("Imprimir diccionario")
+print("----------------------")
+
+print(estudiante)
+
+print("Nombre Antes")
+print(estudiante.get("Name"))
+print("Nombre Despues")
+estudiante.update({"Name":"raul"})
+print(estudiante["Name"])
+
+print("Agregar otro valor al diccionario")
+estudiante["Apellido"]="perez"
+print(estudiante["Apellido"])
+
+
+
+print(estudiante.keys())
+print(estudiante.values())
+
+
+
+
+numeroEstudiantes=[
+    {"name":"jhon","Tipo_Doc":"Cedula","Num_Doc": 99999999,"Direccion": None},
+    {"name":"raul","Tipo_Doc":"T.I","Num_Doc": 88888888,"Direccion": "Carrera 40"},
+    {"name":"perez","Tipo_Doc":"NIT","Num_Doc": 7777777,"Direccion": "Carrera 48"}
+]
+print("Arreglo de Diccionarios")
+print(numeroEstudiantes)
+print(numeroEstudiantes[0].get("name"))
+
+username = input("Ingrese su nombre:")
+print(username)
+
+#Manejo de argumentos variables
+def var_args(name, *args):
+    print(name)
+    print(args)
+
+var_args("Jhon","Perez",985361, None,True,False)
+
+#Manejo de argumentos variables pero con identificacion
+def var_args_def(name, **kwargs):
+    print(name)
+    print(kwargs["Apellido"],kwargs["Identificacion"],kwargs["Feedback"],kwargs["Suscripcion"])
+
+var_args_def("Jhon",Apellido="Perez",Identificacion=985361, Feedback=None,Suscripcion=True)
---
+++ functions.py
@@ -0,0 +1,65 @@
+import os
+
+students = []
+
+def get_students_titlecase():
+    students_titlecase = []
+    for student in students:
+        students_titlecase.append(student["name"].title())
+    return students_titlecase
+
+
+def print_students_titlecase():
+    students_titlecase = get_students_titlecase()
+    print(students_titlecase)
+
+def add_students(name,student_id=332):
+    student = {"name":name,"student_id":student_id}
+    students.append(student)
+
+
+def save_file(student):
+    try:
+        f=open("students.txt","a")
+        f.write(student + "\n")
+        f.close()
+    except Exception:
+        print("Could Not Save File")
+
+def read_file():
+    try:
+        f=open("students.txt","r")
+        for student in f.readlines():
+            add_students(student)
+        f.close()
+
+    except Exception:
+        print("Could Not Read File")
+
+read_file()
+print_students_titlecase()
+
+
+bandera= True
+while bandera:
+    print("Ingresar Datos del Estudiante")
+    student_name = input("Enter student name: ")
+    student_id = input("Enter student ID: ")
+    add_students(student_name,student_id)
+    save_file(student_name)
+    
+    continuarIngresandoEstudiantes = input("Deseda seguir ingresando estudiantes (S) ó (N) :")
+
+    if (continuarIngresandoEstudiantes=="S"):
+        bandera=True
+        os.system("cls")
+        
+    else:
+        bandera=False
+        print("Lista de Estudiantes")
+        print_students_titlecase()
+
+
+
+
+
