4618b04f915ba449b7e72d086c71fba2702afe2a
jrperez <jrperez@bancolombia.com.co>
2021-04-27 06:41:40 -0500

    27Abr2021 06:40

--- API_REST_WEB/app/categoria.py
+++ API_REST_WEB/app/categoria.py
@@ -3,23 +3,44 @@ from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__) #Instacia la aplicacion
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3386/bdpythonapi' #String de conenxion
+app = Flask(__name__) #Instancia la aplicacion
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3386/bdpythonapi' #String de conexion
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #Deshabilita la gestión de notificaciones de sqlalchemy
 
-db = SQLAlchemy(app) #rear un objeto que representa nuestra base de datos
+db = SQLAlchemy(app) #Crear un objeto que representa nuestra base de datos
 ma = Marshmallow(app)
 
+#Creacion Tabla Categoria
 class Categoria(db.model):
     cat_id = db.Column(db.Integer, primary_key=True)
     cat_nom = db.Column(db.String(100))
     cat_desp = db.Column(db.String(100))
 
-    def __init_(self,cat_nom,cat_desp):
+    def __init__(self,cat_nom,cat_desp):
         self.cat_nom = cat_nom
         self.cat_desp = cat_desp
 
 db.create_all()
+
+#Esquema Categoria
+class CategoriaSchema(ma.Schema):
+    class Meta:
+        fields = ('cat_id'.'cat_mom','cat_desp')
+
+#Una sola respuesta
+categoria_schema =  CategoriaSchema()
+
+#Varias respuestas
+categorias_schema = CategoriaSchema(many = True)
+
+#GET
+@app.route('/categoria',methods=['GET'])
+def get_categoria():
+    all_categorias = Categoria.query.all()
+    result = categorias_schema.dump(all_categorias)
+    return jsonify(result)
+
+
 
 #Mensaje de Bienvenida
 @app.route('/',methods=['GET'])
