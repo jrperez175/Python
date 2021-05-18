62cd6245ae384399651743ab18e102b90fa77e63
Jhon Perez <jrperez175@gmail.com>
2021-05-18 06:24:39 -0500

    18May2021 06:20

---
+++ UniversidadPythonUdemy/Seccion02Variables/01-06-00-DireccionMemoriaVariables-UP.py
@@ -0,0 +1,10 @@
+x = 10
+y = 2
+z = x + y
+print(x)
+print(y)
+print(z)
+print(id(x)) #Id almacena la posicion de memoria donde esta almacenado el valor
+print(id(y))
+print(id(z))
+print(type(x))
---
+++ UniversidadPythonUdemy/Seccion03TiposDeDatos/01-07-00-TiposDatos-UP.py
@@ -0,0 +1,24 @@
+y = 10
+x = False
+z: str = 'Hola Mundo' #Nos da una pista del valor almacenado en la variable, ¡¡¡ OJO PERO NO LA TIPEA ES INFORMATIVO!!!
+s = 10.3
+
+#Todos los tipos de datos a su vez estan contenidas en clases
+
+print(f'El valor de x es: {x}')
+print(f'El tipo de dato de x es: {type(x)}')
+
+print('El valor de x es: {}'.format(x))
+print(f'El tipo de dato de x es: {type(x)}')
+
+
+print(f'El valor de y es: {y}')
+print(f'El tipo de dato de y es: {type(y)}')
+
+print(f'El valor de z es: {z}')
+print(f'El tipo de dato de z es: {type(z)}')
+
+print(f'El valor de s es: {s}')
+print(f'El tipo de dato de s es: {type(s)}')
+
+
