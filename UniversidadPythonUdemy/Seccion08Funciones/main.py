be481fe383f140dbdc92ba71963f6ca1279b8dc4
jrperez <jrperez@bancolombia.com.co>
2021-06-10 07:15:54 -0500

    10Jun2021 07:15

--- UniversidadPythonUdemy/Seccion08Funciones/main.py
+++ UniversidadPythonUdemy/Seccion08Funciones/main.py
@@ -19,9 +19,14 @@ def sumarConParametrosDefault(a=5, b=7):
 
 
 # Funciones sin definir numero de parametros
-def listarNombres(*nombres):
+def listarNombres(*nombres): #Usualmente en la literatura se utiliza la expresion def fun(*arg):
     for nombre in nombres:
         print(nombre)
+
+# Funciones que reciben Diccionarios
+def listarTerminos(**terminos): #Usualmente en la literatura se utiliza la expresion def fun(**kwarg):
+    for llave , valor in terminos.items():
+        print(f'la llave es: {llave} y el valor es: {valor}')
 
 # Funciones con parametros llave-valor
 
@@ -30,8 +35,12 @@ if __name__ == "__main__":
     # miFuncionParametros('Jhon', 'Raul')
     # miFuncionParametros('Karla', 'Lara')
     resultado = sumar(5, 3);
-    print(f'El resultado de la suma es: {resultado}')
-    print(f'El resultado de la suma es: {sumar(5, 5)}')
-    print(f'El resultado de la suma es: {sumarConParametrosDefault()}')
-    listarNombres('Jhon','Raul')
-    listarNombres('Jhon','Raul','Perez','Munoz')
+    #print(f'El resultado de la suma es: {resultado}')
+    #print(f'El resultado de la suma es: {sumar(5, 5)}')
+    #print(f'El resultado de la suma es: {sumarConParametrosDefault()}')
+    #listarNombres('Jhon','Raul')
+    #listarNombres('Jhon','Raul','Perez','Munoz')
+    listarTerminos(IDE='Integrated Developement Enviroment'
+                   ,PK='Primary Key'
+                   ,PI=3.1416) #La llave del diccionario no lleva comillas y el valor puede se cualquier dato
+    listarTerminos(DBMS='Database Management System')
