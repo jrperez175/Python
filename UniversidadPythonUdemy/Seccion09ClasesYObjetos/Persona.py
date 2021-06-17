e4c67583077009b1645146af2582d599795424db
jrperez <jrperez@bancolombia.com.co>
2021-06-17 07:04:01 -0500

    17Jun2021 07:00

---
+++ UniversidadPythonUdemy/Seccion09ClasesYObjetos/Persona.py
@@ -0,0 +1,10 @@
+class Persona:
+    def __init__(self, nombre, apellido, edad):
+        self.nombre = nombre #Atributos de clase
+        self.apellido = apellido
+        self.edad = edad
+        self.sexo = 'M' #Se pueden definir atributos fijos
+
+#print(type(Persona))
+persona1 = Persona('Jhon','Perez',49) ##Llama al constructor de la clase, indirectamente es el metodo init
+print(f'Mi nombre es: {persona1.nombre} , mi apellido es {persona1.apellido} , mi edad es {persona1.edad} y mi sexo es {persona1.sexo}')
