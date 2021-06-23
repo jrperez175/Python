af605a82d15a50b2ae4fc9fdcddb0bae1aeb9ed0
jrperez <jrperez@bancolombia.com.co>
2021-06-23 06:56:26 -0500

    23Jun2021 07:00

--- UniversidadPythonUdemy/Seccion09ClasesYObjetos/Persona.py
+++ UniversidadPythonUdemy/Seccion09ClasesYObjetos/Persona.py
@@ -5,10 +5,28 @@ class Persona:
         self.edad = edad
         self.sexo = 'M'  # Se pueden definir atributos fijos
 
+    def mostrar_detalle(self):
+        print(f'Persona: {self.nombre} {self.apellido} {self.edad}')
 
-# print(type(Persona))
+print(type(Persona))
+print(f'---------------------------------------')
+print(f'Imprimiendo con un variable que contienen el objeto')
 persona1 = Persona('Jhon', 'Perez', 49)  ##Llama al constructor de la clase, indirectamente es el metodo init
 print(f'Mi nombre es: {persona1.nombre} , mi apellido es {persona1.apellido} , mi edad es {persona1.edad} y mi sexo es {persona1.sexo}')
 
 persona2 = Persona('Raul', 'Muñoz', 49)  ##Llama al constructor de la clase, indirectamente es el metodo init
 print(f'Mi nombre es: {persona2.nombre} , mi apellido es {persona2.apellido} , mi edad es {persona2.edad}')
+
+#Modificar valores del objeto
+print(f'---------------------------------------')
+print(f'Modificando valores de los objetos')
+persona1.nombre = "Jhon Raul"
+persona1.apellido = 'Perez Muñoz'
+print(f'Mi nombre es: {persona1.nombre} , mi apellido es {persona1.apellido} , mi edad es {persona1.edad} y mi sexo es {persona1.sexo}')
+
+
+#Metodos de Instancia
+print(f'---------------------------------------')
+print(f'Imprimiendo con un medodo de instancia')
+persona1.mostrar_detalle()
+persona2.mostrar_detalle()
